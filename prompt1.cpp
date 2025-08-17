e4219a334a2d6014e815de5a9eca814d154293ca
sprogma <mux-228@yandex.ru>
2025-08-17 22:14:28 +0300

    hide copies of code under define

--- prompt1.cpp
+++ prompt1.cpp
@@ -2,6 +2,20 @@
 #include "prompt1.h"
 #include "read_double.h"
 
+
+#define CHECK_RETURN_VALUE(result_code) \
+    do { \
+        if (res == READ_COEFFICIENTS_EOF) \
+        { \
+            return READ_COEFFICIENTS_EOF; \
+        } \
+        if (res != 0) \
+        { \
+            return 1; \
+        }\
+    } while (0)
+
+
 /* v1 : copy */
 
 int read_coefficients_copy(struct coefficients_t *coeff)
@@ -11,34 +25,13 @@ int read_coefficients_copy(struct coefficients_t *coeff)
     const char * const format_string = "Enter coefficient at x^%d [%s] > ";
     
     res = read_double(&coeff->c, format_string, 2, "c");
-    if (res == READ_COEFFICIENTS_EOF) 
-    { 
-        return READ_COEFFICIENTS_EOF; 
-    }
-    if (res != 0) 
-    { 
-        return 1; 
-    }
+    CHECK_RETURN_VALUE(res);
     
     res = read_double(&coeff->b, format_string, 1, "b");
-    if (res == READ_COEFFICIENTS_EOF) 
-    { 
-        return READ_COEFFICIENTS_EOF; 
-    }
-    if (res != 0) 
-    { 
-        return 1; 
-    }
+    CHECK_RETURN_VALUE(res);
     
     res = read_double(&coeff->a, format_string, 0, "a");
-    if (res == READ_COEFFICIENTS_EOF) 
-    { 
-        return READ_COEFFICIENTS_EOF; 
-    }
-    if (res != 0) 
-    { 
-        return 1; 
-    }
+    CHECK_RETURN_VALUE(res);
 
     return 0;
 
