097d122cf2a437f1a63747e77ff59ead4079800f
sprogma <mux-228@yandex.ru>
2025-08-17 23:03:22 +0300

    split print_result on 2 functions

--- output.cpp
+++ output.cpp
@@ -7,49 +7,62 @@
 #include "color.h"
 
 
-int print_result(double *roots, enum solution_result_codes result_code)
+int print_result_color(const double *roots, enum solution_result_codes result_code);
+int print_result_text(const double *roots, enum solution_result_codes result_code);
+
+int print_result_color(const double *roots, enum solution_result_codes result_code)
+{
+    switch (result_code)
+    {
+        case RESULT_0_SOLUTIONS:
+            printf(COLOR_RED("There is no solutions.") "\n" );
+            break;
+        case RESULT_1_SOLUTIONS:
+            printf(COLOR_GREEN("The only solution is: %g") "\n", roots[0]);
+            break;
+        case RESULT_2_SOLUTIONS:
+            printf(COLOR_GREEN("The solutions is both %g and %g")"\n", roots[0], roots[1]);
+            break;
+        case RESULT_INFINITE_SOLUTIONS:
+            printf(COLOR_YELLOW("Any number is solution")"\n");
+            break;
+        default:
+            printf(COLOR_RED("Wrong result code.")"\n");
+            return 1;
+    }
+    return 0;
+}
+
+int print_result_text(const double *roots, enum solution_result_codes result_code)
+{
+    switch (result_code)
+    {
+        case RESULT_0_SOLUTIONS:
+            break;
+        case RESULT_1_SOLUTIONS:
+            printf("%g\n", roots[0]);
+            break;
+        case RESULT_2_SOLUTIONS:
+            printf("%g\n%g\n", roots[0], roots[1]);
+            break;
+        case RESULT_INFINITE_SOLUTIONS:
+            printf("inf");
+            break;
+        default:
+            return 1;
+    }
+    return 0;
+}
+
+int print_result(const double *roots, enum solution_result_codes result_code)
 {
     if (is_out_terminal())
     {
-        switch (result_code)
-        {
-            case RESULT_0_SOLUTIONS:
-                printf(COLOR_RED("There is no solutions.") "\n" );
-                break;
-            case RESULT_1_SOLUTIONS:
-                printf(COLOR_GREEN("The only solution is: %g") "\n", roots[0]);
-                break;
-            case RESULT_2_SOLUTIONS:
-                printf(COLOR_GREEN("The solutions is both %g and %g")"\n", roots[0], roots[1]);
-                break;
-            case RESULT_INFINITE_SOLUTIONS:
-                printf(COLOR_YELLOW("Any number is solution")"\n");
-                break;
-            default:
-                printf(COLOR_RED("Wrong result code.")"\n");
-                return 1;
-        }
+        return print_result_color(roots, result_code);
     }
     else
     {
-        switch (result_code)
-        {
-            case RESULT_0_SOLUTIONS:
-                break;
-            case RESULT_1_SOLUTIONS:
-                printf("%g\n", roots[0]);
-                break;
-            case RESULT_2_SOLUTIONS:
-                printf("%g\n%g\n", roots[0], roots[1]);
-                break;
-            case RESULT_INFINITE_SOLUTIONS:
-                printf("inf");
-                break;
-            default:
-                return 1;
-        }
+        return print_result_text(roots, result_code);
     }
-
-    return 0;
 }
 
--- output.h
+++ output.h
@@ -6,7 +6,7 @@
 #include "solver.h"
 
 
-int print_result(double *roots, enum solution_result_codes result_code);
+int print_result(const double* roots, enum solution_result_codes result_code);
 
 
 #endif
