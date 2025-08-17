1dd45da258cd9393fa1e8975cff6ae631f8db639
sprogma <mux-228@yandex.ru>
2025-08-17 21:31:10 +0300

    fix check

--- read_double.cpp
+++ read_double.cpp
@@ -13,16 +13,16 @@ const int READ_COEFFICIENTS_EOF = -1;
 
 int read_double(double *result, const char *fmt, ...)
 {
-    va_list args;
-    va_start(args, fmt);
-    
     int ch, scan_result;
     *result = 0.0;
     while (1)
     {
         if (is_in_terminal())
         {        
+            va_list args;
+            va_start(args, fmt);
             vfprintf(stderr, fmt, args);
+            va_end(args);
         }
         scan_result = scanf("%lg", result);
         if (scan_result == 1)
@@ -31,17 +31,14 @@ int read_double(double *result, const char *fmt, ...)
         }
         if (scan_result == EOF)
         {
-            va_end(args);
             return READ_COEFFICIENTS_EOF;
         }
         do { ch = getchar(); } 
         while (ch != EOF && ch != '\n');
         if (ch == EOF)
         {
-            va_end(args);
             return READ_COEFFICIENTS_EOF;
         }
     }
-    va_end(args);
     return 0;
 }
